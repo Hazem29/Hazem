0cd25e3f6540839dcd009584e1a0e83a54e66dc0
Hazem29 <85832654+Hazem29@users.noreply.github.com>
2023-05-26 16:54:15 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "hbana29@gmail.com";
+$sitename = "Hazem";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
